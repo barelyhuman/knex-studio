180cf3132aaeb37c2a2fc1b6281cd0937f5b18b9
reaper <ahoy@barelyhuman.dev>
2024-12-17 22:22:22 +0530

    fix: always generate metafile

--- src/plugins/view.js
+++ src/plugins/view.js
@@ -1,92 +1,87 @@
-import fs from 'node:fs'
-import { dirname } from 'node:path'
-import { fileURLToPath } from 'node:url'
-import { join } from 'path'
-import { build } from 'esbuild'
-import fp from 'fastify-plugin'
-import glob from 'tiny-glob'
+import fs from "node:fs";
+import { dirname } from "node:path";
+import { fileURLToPath } from "node:url";
+import { join } from "path";
+import { build } from "esbuild";
+import fp from "fastify-plugin";
+import glob from "tiny-glob";
 
-const __dirname = dirname(fileURLToPath(import.meta.url))
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default fp(
   (f, o, d) => {
-    let metafile
-    f.addHook('onReady', async () => {
+    let metafile;
+    f.addHook("onReady", async () => {
       const hasFile = await fs.promises
-        .access(join(__dirname, '../metafile.js'))
+        .access(join(__dirname, "../metafile.js"))
         .then(() => true)
-        .catch(() => false)
+        .catch(() => false);
 
-      const isDevMode = process.env.NODE_ENV != 'production'
+      const assets = await glob("**/*.{css,js}", {
+        cwd: join(__dirname, "../assets"),
+        filesOnly: true,
+        absolute: true,
+      });
 
-      // Keep generating it for dev mode based on changes.
-      if (!hasFile || isDevMode) {
-        const assets = await glob('**/*.{css,js}', {
-          cwd: join(__dirname, '../assets'),
-          filesOnly: true,
-          absolute: true,
-        })
+      const output = await build({
+        entryPoints: assets,
+        format: "esm",
+        bundle: true,
+        metafile: true,
+        outdir: join(__dirname, "../_public"),
+        treeShaking: true,
+        splitting: true,
+      });
 
-        const output = await build({
-          entryPoints: assets,
-          format: 'esm',
-          bundle: true,
-          metafile: true,
-          outdir: join(__dirname, '../_public'),
-          treeShaking: true,
-          splitting: true,
-        })
+      await fs.promises.writeFile(
+        join(__dirname, "../metafile.js"),
+        `export default ${JSON.stringify(output.metafile, null, 2)}`,
+        "utf8"
+      );
 
-        await fs.promises.writeFile(
-          join(__dirname, '../metafile.js'),
-          `export default ${JSON.stringify(output.metafile, null, 2)}`,
-          'utf8'
-        )
-      }
+      metafile = (await import("../metafile.js")).default;
+    });
 
-      metafile = (await import('../metafile.js')).default
-    })
-
-    f.decorateReply('view', function (template, args) {
+    f.decorateReply("view", function (template, args) {
       return this._view(template, {
         ...args,
-        linkStyle: file => {
+        linkStyle: (file) => {
           if (!metafile.inputs[file]) {
             throw new Error(
               `cannot \`inlineScript\` since the provided asset doesn't exisst ${scriptName}`
-            )
+            );
           }
 
           for (let cssOut in metafile.outputs) {
             if (metafile.outputs[cssOut].entryPoint === file) {
               return `<link rel="stylesheet" href="${cssOut.replace(
-                'src/',
-                '/'
-              )}">`
+                "src/",
+                "/"
+              )}">`;
             }
           }
         },
-        linkScript: scriptName => {
+        linkScript: (scriptName) => {
           if (!metafile.inputs[scriptName]) {
             throw new Error(
               `cannot \`inlineScript\` since the provided asset doesn't exisst ${scriptName}`
-            )
+            );
           }
 
           for (let script in metafile.outputs) {
             if (metafile.outputs[script].entryPoint === scriptName) {
               return `<script type="module" src="${script.replace(
-                'src/',
-                '/'
-              )}"></script>`
+                "src/",
+                "/"
+              )}"></script>`;
             }
           }
         },
-      })
-    })
-    d()
+      });
+    });
+    d();
   },
   {
-    name: 'view',
+    name: "view",
   }
-)
+);
